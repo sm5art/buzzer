25bcbdde3f78e74fb443eee69731de084d2019ee
Artur Kashperskiy <art@network.ru>
2017-01-18 19:59:39 -0800

    wow

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.jsx?$/, exclude: /node_modules/, query: {presets: ['es2015', 'react', 'stage-1']}, loader: "babel-loader"},
+      { test: /\.jsx?$/, exclude: /node_modules/, query: {presets: [['es2015'], ['react'], ['stage-1']]}, loader: "babel-loader"},
       {
         test: /\.css$/,
         exclude: /node_modules/,
